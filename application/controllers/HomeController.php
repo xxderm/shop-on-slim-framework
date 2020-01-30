d819984bb495d8ba499d79ae9d62942af912f26d
Muratshin Elya <ptrwowhochy@gmail.com>
2020-01-30 20:12:52 +0500

    fix variable name

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -2,16 +2,16 @@
 include_once dirname(__DIR__)."\models\DB connection.php";
 class HomeController
 {
-    public $m_twig;
+    public $m_twig_container;
     function __construct($twig)
     {
-        $this->m_twig = $twig;
+        $this->m_twig_container = $twig;
     }
     public function index($req, $resp, $arg)
     {
         $db = connection::getInstance();
         $data = $db->getConnection()->products;
-        $template = $this->m_twig['twig_c']->loadTemplate('ProductsPage.html');
+        $template = $this->m_twig_container['twig_c']->loadTemplate('ProductsPage.html');
         echo $template->render(
             array(
                 'prod' => $data,
