634995d82c37c3d2547314766fff9705eaa85843
Muratshin Elya <ptrwowhochy@gmail.com>
2020-01-30 20:10:28 +0500

    placed twig to slim container

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -2,22 +2,21 @@
 include_once dirname(__DIR__)."\models\DB connection.php";
 class HomeController
 {
-    public static function index($req, $resp, $arg)
+    public $m_twig;
+    function __construct($twig)
+    {
+        $this->m_twig = $twig;
+    }
+    public function index($req, $resp, $arg)
     {
         $db = connection::getInstance();
         $data = $db->getConnection()->products;
-
-
-        Twig_Autoloader::register();
-        $loader = new Twig_Loader_Filesystem('application/views');
-        $twig = new Twig_Environment($loader);
-        $template = $twig->loadTemplate('ProductsPage.html');
+        $template = $this->m_twig['twig_c']->loadTemplate('ProductsPage.html');
         echo $template->render(
             array(
                 'prod' => $data,
                 'title' => 'Production'
                 ));
-
         return $resp;
     }
     public static function fromCatalog($req, $resp, $arg)
--- index.php
+++ index.php
@@ -1,11 +1,26 @@
 <?php
 include_once "application/controllers/HomeController.php";
 require 'vendor/autoload.php';
+require 'vendor/psr/http-message/src/ServerRequestInterface.php';
+require 'vendor/psr/http-message/src/ResponseInterface.php';
 
 $app = new Slim\App();
 
+$container = $app->getContainer();
+$container['twig_c'] = function ($c)
+{
+    Twig_Autoloader::register();
+    $loader = new Twig_Loader_Filesystem('application/views');
+    $twig = new Twig_Environment($loader);
+    return $twig;
+};
+$container['HomeController'] = function ($c)
+{
+    return new HomeController($c['twig_c']);
+};
+
 # Home page
-$app->get('/', HomeController::class . ':index');
-$app->get('/catalog/{id}', HomeController::class . ':fromCatalog');
+$app->get('/', '\HomeController:index');
+$app->get('/catalog/{id}', '\HomeController:fromCatalog');
 
 $app->run();
