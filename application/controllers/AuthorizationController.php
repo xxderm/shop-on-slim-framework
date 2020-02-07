4467b819cc595b8768f3353402bf6f2f92a5eab1
Muratshin Elya <ptrwowhochy@gmail.com>
2020-02-07 15:08:33 +0500

    fix

--- application/controllers/AuthorizationController.php
+++ application/controllers/AuthorizationController.php
@@ -1,20 +1,25 @@
 <?php
+include_once dirname(__DIR__)."\models\DB connection.php";
 class AuthorizationController
 {
     public $m_twig_container;
     public $m_nav_container;
+    public $db;
     function __construct($container)
     {
         $this->m_twig_container = $container['twig_c'];
         $this->m_nav_container = $container['nav_bar'];
+        $this->db = connection::getInstance();
     }
     public function getSignUp($req, $resp, $arg)
     {
         $template = $this->m_twig_container->loadTemplate('SignUp.html');
+        $catalogName = $this->db->getConnection()->catalog;
         echo $template->render(
             array(
                 'title' => 'SignUp',
-                'nav_list' => $this->m_nav_container
+                'nav_list' => $this->m_nav_container,
+                'catalog_buff' => $catalogName
             ));
         return $resp;
     }
@@ -26,10 +31,12 @@ class AuthorizationController
     public function getSignIn($req, $resp, $arg)
     {
         $template = $this->m_twig_container->loadTemplate('SignIn.html');
+        $catalogName = $this->db->getConnection()->catalog;
         echo $template->render(
             array(
                 'title' => 'SignIn',
-                'nav_list' => $this->m_nav_container
+                'nav_list' => $this->m_nav_container,
+                'catalog_buff' => $catalogName
             )
         );
         return $resp;
